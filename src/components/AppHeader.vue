6dbe1e7fb5fc3389fa4ca5eb59857d9f109228d9
Marco 9726 <marcosarra9@gmail.com>
2023-01-17 20:16:17 +0100

    create AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<script>
+    export default {
+      data(){
+        return{
+          logoUrl: 'https://i0.wp.com/www.downloadfonts.io/wp-content/uploads/2021/09/Netflix-Logo-Font.png?fit=1024%2C581&ssl=1'
+        }
+      }
+    }
+</script>
+
+<template>
+  <header>
+    <div class="my-container">
+      <img :src="`${this.logoUrl}`" alt="" class="h-100">
+    </div>
+  </header>
+
+</template>
+<style lang="scss">
+  @use '../scss/generals.scss' as *;
+  header{
+    height: 80px;
+    background-color: black;
+  }
+
+</style>
