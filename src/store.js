55a1148cbf7484131cbb5374b0235b6e1f4f27f2
Marco 9726 <marcosarra9@gmail.com>
2023-01-18 15:15:37 +0100

    create file store.js

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { reactive } from 'vue';
+
+export const store = reactive({
+    // base url movies 
+    urlMovies: 'https://api.themoviedb.org/3/search/movie?api_key=e3ed1f013cf6f646f124a113802c7b5c&query=',
+    arrayMovies: [],
+    // base url seies
+    urlSeries: 'https://api.themoviedb.org/3/search/tv?api_key=e3ed1f013cf6f646f124a113802c7b5c&query=',
+    arraySeries: []
+})
