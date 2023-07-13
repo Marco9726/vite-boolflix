0c48e6bae3ae3a196b280ebbcf19459efddbe0ba
Marco 9726 <marcosarra9@gmail.com>
2023-07-13 18:46:44 +0200

    responsive adjustments

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -1,90 +1,122 @@
 <script>
-    import { store } from '../store.js';
-    import axios from 'axios';
-    export default {
-        data(){
-            return{
-                store,
-                path: 'http://image.tmdb.org/t/p/w1280/',
-            }
-        },
-        methods: {
-            setDate(){
-                if(store.objectActive.release_date){
-                    return store.objectActive.release_date.split('-')[0]
-                }
-                else{
-                    return store.objectActive.first_air_date.split('-')[0]
-                }
-            }
-        }
-    }
+import { store } from '../store.js';
+import axios from 'axios';
+export default {
+   data() {
+      return {
+         store,
+         path: 'http://image.tmdb.org/t/p/w1280/',
+      };
+   },
+   methods: {
+      setDate() {
+         if (store.objectActive.release_date) {
+            return store.objectActive.release_date.split('-')[0];
+         } else {
+            return store.objectActive.first_air_date.split('-')[0];
+         }
+      },
+   },
+};
 </script>
 
 <template>
-    <!-- visibile solo se almeno un array contenga almeno un contenuto  -->
-    <div id="jumbotron" v-if="store.arrayMovies.length > 0 || store.arraySeries.length > 0">
-        <img :src="this.path+`${store.objectActive.backdrop_path}` " :alt="`${store.objectActive.original_title}`">
-        <div class="my-container d-flex align-items-center text-white">
-            <div id="descritions" class="w-50">
-                <h1 v-if="(store.objectActive.title)">{{ store.objectActive.title }}</h1> 
-                <h1 v-else >{{ store.objectActive.name }}</h1>
-                <p>{{ store.objectActive.overview }}</p>
-                <span class="date">{{ setDate() }}</span>
-                <div class="cast">
-                    <h5 class="mb-0 mt-3">Cast:</h5>
-                    <ul class="d-flex p-0">
-                        <!-- ciclo i 5 membri del cast dell'arrayCast  -->
-                        <li v-for="(item,index) in store.arrayCast" :key="index" class="small-text">{{ item.name }}</li>
-                    </ul>
-                </div>
-                <div class="genres">
-                    <ul class="d-flex p-0">
-                        <!-- ciclo i generi nell'arrayGenres  -->
-                        <li v-for="(item,index) in store.arrayGenres" :key="index">{{ item.name }}</li>
-                    </ul>
-                </div>
+   <!-- visibile solo se almeno un array contenga almeno un contenuto  -->
+   <div
+      id="jumbotron"
+      v-if="store.arrayMovies.length > 0 || store.arraySeries.length > 0"
+   >
+      <img
+         :src="this.path + `${store.objectActive.backdrop_path}`"
+         :alt="`${store.objectActive.original_title}`"
+      />
+      <div class="my-container d-flex align-items-center text-white">
+         <div id="descriptions">
+            <h1 v-if="store.objectActive.title">
+               {{ store.objectActive.title }}
+            </h1>
+            <h1 v-else>{{ store.objectActive.name }}</h1>
+            <p>{{ store.objectActive.overview }}</p>
+            <span class="date">{{ setDate() }}</span>
+            <div class="cast">
+               <h5 class="mb-0 mt-3">Cast:</h5>
+               <ul class="d-flex p-0">
+                  <!-- ciclo i 5 membri del cast dell'arrayCast  -->
+                  <li
+                     v-for="(item, index) in store.arrayCast"
+                     :key="index"
+                     class="small-text"
+                  >
+                     {{ item.name }}
+                  </li>
+               </ul>
             </div>
-
-        </div>
-    </div>
+            <div class="genres">
+               <ul class="d-flex p-0">
+                  <!-- ciclo i generi nell'arrayGenres  -->
+                  <li v-for="(item, index) in store.arrayGenres" :key="index">
+                     {{ item.name }}
+                  </li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   </div>
 </template>
 
 <style lang="scss">
-  @use '../scss/generals.scss' as *;
-  @use '../scss/partials/variables' as *;
+@use '../scss/generals.scss' as *;
+@use '../scss/partials/variables' as *;
 
-  #jumbotron{
-    height: 66vh;
-    position: relative;
-    img{
-        object-fit: cover;
-        object-position: center;
-        height: 100%;
-        width: 100%;
-    }
+#jumbotron {
+   height: 66vh;
+   position: relative;
+   img {
+      object-fit: cover;
+      object-position: center;
+      height: 100%;
+      width: 100%;
+   }
 
-    .my-container{
-        position: absolute;
-        bottom: 0;
-        left: 3.5%;
-        text-shadow: black 0 0 8px;
-        #descritions{
-            p{
-                font-size: $small;
+   .my-container {
+      position: absolute;
+      bottom: 0;
+      left: 3.5%;
+      text-shadow: black 0 0 8px;
+      #descriptions {
+         overflow-y: scroll;
+         max-height: 60%;
+         width: 55%;
+         p {
+            font-size: $small;
+            overflow-y: scroll;
+            max-height: 60%;
+         }
+
+         ul {
+            li:not(:last-child) {
+               margin-right: 0.5rem;
+               &::after {
+                  content: ',';
+               }
             }
-
-            ul{
-                li:not(:last-child){
-                    margin-right: .5rem;
-                    &::after{
-                        content: ',';
-                    }
-                    
-                }
-            }
-        }
-    }
-  }
-    
+         }
+      }
+   }
+}
+//breakpoints
+@media (max-width: 768px) {
+   html {
+      font-size: 12px;
+   }
+   #jumbotron {
+      height: 50vh;
+      font-size: 0.9em;
+      .my-container {
+         #descriptions {
+            width: 75%;
+         }
+      }
+   }
+}
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,74 +1,103 @@
 <script>
-    export default {
-        props:{
-            card: Object
-        },
-        data(){
-            return{
-                path: 'http://image.tmdb.org/t/p/w780/'
-            }
-        },
-        methods: {
-            //metodo per calcolare le stelle piene in base al voto
-            filledStars(){
-                return Math.round(this.card.vote_average / 2);
-            }       
-        }          
-    }
+export default {
+   props: {
+      card: Object,
+   },
+   data() {
+      return {
+         path: 'http://image.tmdb.org/t/p/w780/',
+      };
+   },
+   methods: {
+      //metodo per calcolare le stelle piene in base al voto
+      filledStars() {
+         return Math.round(this.card.vote_average / 2);
+      },
+   },
+};
 </script>
 
 <template>
-    <div class="my-card">
-        <!-- compongo il path con la proprietà 'poster_path' della prop 'card' per visualizzare l'immagine -->
-        <img :src="this.path+`${card.poster_path}`" :alt="`${card.title}`" class="w-100 h-100 rounded-1">
-        <div class="info title text-white rounded-1">
-            <h5 v-if="(card.title)">{{ card.title }}</h5> 
-            <h5 v-else >{{ card.name }}</h5>
-            <div class="stars">
-                <!-- creo stelle piene in base al numero calcolato nei methods  -->
-                <i v-for="(item,index) in filledStars()" class="fa-solid fa-star" :key="index" :star="item"></i>
-                <!-- sottraggo il numero di stelle piene a 5 per calcolare le restanti vuote  -->
-                <i v-for="(item,index) in (5 - filledStars())" class="fa-regular fa-star" :key="index" :star="item"></i>
-            </div>
-        </div> 
-
-    </div>
+   <div class="my-card col-4 col-sm-3 col-md-2">
+      <!-- compongo il path con la proprietà 'poster_path' della prop 'card' per visualizzare l'immagine -->
+      <img
+         :src="this.path + `${card.poster_path}`"
+         :alt="`${card.title}`"
+         class="w-100 h-100 rounded-1"
+      />
+      <div class="info title text-white rounded-1">
+         <h5 v-if="card.title">{{ card.title }}</h5>
+         <h5 v-else>{{ card.name }}</h5>
+         <div class="stars">
+            <!-- creo stelle piene in base al numero calcolato nei methods  -->
+            <i
+               v-for="(item, index) in filledStars()"
+               class="fa-solid fa-star"
+               :key="index"
+               :star="item"
+            ></i>
+            <!-- sottraggo il numero di stelle piene a 5 per calcolare le restanti vuote  -->
+            <i
+               v-for="(item, index) in 5 - filledStars()"
+               class="fa-regular fa-star"
+               :key="index"
+               :star="item"
+            ></i>
+         </div>
+      </div>
+   </div>
 </template>
 
 <style lang="scss">
- @use '../scss/generals.scss' as *;
+@use '../scss/generals.scss' as *;
 
-    .my-card{
-        min-width: calc(100% / 6 - .5rem);
-        height: 300px;
-        margin-right: .5rem;
-        margin-bottom: 5px;
-        cursor: pointer;
-        position: relative;
+.my-card {
+   //   min-width: calc(100% / 6 - .5rem);
+   height: 300px;
+   margin-right: 0.5rem;
+   margin-bottom: 5px;
+   cursor: pointer;
+   position: relative;
 
-        img{
-            object-fit: cover;
-            object-position: center;
-        }
-        .info.title{
-            width: 100%;
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            padding: 1rem .5rem;
-            line-height: .75rem;
-            background: rgb(0,0,0);
-            background: linear-gradient(3deg, rgba(0,0,0,1) 20%, rgba(0,0,0,0) 91%);
-            display: none;
+   img {
+      object-fit: cover;
+      object-position: center;
+   }
+   .info.title {
+      width: 100%;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      padding: 1rem 0.5rem;
+      line-height: 0.75rem;
+      background: rgb(0, 0, 0);
+      background: linear-gradient(
+         3deg,
+         rgba(0, 0, 0, 1) 20%,
+         rgba(0, 0, 0, 0) 91%
+      );
+      display: none;
 
-            span{
-                text-shadow: 0px 0px 10px rgb(0,0,0);
-            }
-            
-        }
-        //mostro le info all'over sulla card
-        &:hover .info.title{
-            display: block;
-        }
-    }
+      span {
+         text-shadow: 0px 0px 10px rgb(0, 0, 0);
+      }
+   }
+   //mostro le info all'over sulla card
+   &:hover .info.title {
+      display: block;
+   }
+}
+
+//breakpoints
+@media (max-width: 768px) {
+   .my-card {
+      height: 250px;
+   }
+}
+
+@media (max-width: 576px) {
+   .my-card {
+      height: 200px;
+   }
+}
 </style>
